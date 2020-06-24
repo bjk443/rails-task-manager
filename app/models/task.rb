76773025c3bf9788d3c53a6eeeae6995690d421f
john kang <johnkang420@gmail.com>
2020-06-24 13:34:04 +0800

    task manager backend working!

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  validates :title, presence: true
+  validates :details, presence: true
+  validates :completed, inclusion: { in: [true, false] }
+  # validating completed booleans?
 end
